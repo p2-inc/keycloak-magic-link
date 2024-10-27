d8fdd366cf23791f20549e1aae6e7976ae29833d
Garth <244253+xgp@users.noreply.github.com>
2024-10-27 09:55:47 +0100

    port to 26. upgrade lombok. (#90)
    * port to 26. upgrade lombok.
    
    * info logging to debug

--- src/main/java/io/phasetwo/keycloak/magic/resources/MagicLinkResource.java
+++ src/main/java/io/phasetwo/keycloak/magic/resources/MagicLinkResource.java
@@ -79,7 +79,7 @@ public class MagicLinkResource extends AbstractAdminResource {
     boolean sent = false;
     if (sendEmail) {
       sent = MagicLink.sendMagicLinkEmail(session, user, link);
-      log.infof("sent email to %s? %b. Link? %s", rep.getEmail(), sent, link);
+      log.debugf("sent email to %s? %b. Link? %s", rep.getEmail(), sent, link);
     }
 
     MagicLinkResponse resp = new MagicLinkResponse();
