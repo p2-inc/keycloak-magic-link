a5df53e45565dc915971cf622c9f2abe60fa7106
razvantufisi <rtufisi@phasetwo.io>
2025-02-28 13:14:28 +0200

    Fix magic link PKCE

--- src/main/java/io/phasetwo/keycloak/magic/auth/token/MagicLinkActionTokenHandler.java
+++ src/main/java/io/phasetwo/keycloak/magic/auth/token/MagicLinkActionTokenHandler.java
@@ -100,7 +100,7 @@ public class MagicLinkActionTokenHandler extends AbstractActionTokenHandler<Magi
 
     if (token.getScope() != null) {
       authSession.setClientNote(OAuth2Constants.SCOPE, token.getScope());
-      AuthenticationManager.setClientScopesInSession(authSession);
+      AuthenticationManager.setClientScopesInSession(tokenContext.getSession(), authSession);
     }
 
     if (token.getRememberMe() != null && token.getRememberMe()) {
