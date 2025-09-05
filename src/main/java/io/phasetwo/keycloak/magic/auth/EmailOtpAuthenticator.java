f2a847f67796b55024bdff51fb44cf9060d528c1
Robér Hinz <rober.hinz@lichtblick.de>
2025-09-05 12:19:32 +0200

    brute force protection for Email-OTP (#133)
    * trigger brute force protection for invalid otp codes
    
    * correctly handle brute force detection

--- src/main/java/io/phasetwo/keycloak/magic/auth/EmailOtpAuthenticator.java
+++ src/main/java/io/phasetwo/keycloak/magic/auth/EmailOtpAuthenticator.java
@@ -11,9 +11,12 @@ import jakarta.ws.rs.core.Response;
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.extern.jbosslog.JBossLog;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.authenticators.util.AuthenticatorUtils;
 import org.keycloak.events.Errors;
 import org.keycloak.events.EventBuilder;
+import org.keycloak.events.EventType;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
@@ -28,10 +31,10 @@ public class EmailOtpAuthenticator implements Authenticator {
   public static final String FORM_PARAM_OTP_CODE = "otp";
 
   public void authenticate(AuthenticationFlowContext context) {
-    challenge(context, null);
+    challenge(context, null, false);
   }
 
-  private void challenge(AuthenticationFlowContext context, FormMessage errorMessage) {
+  private void challenge(AuthenticationFlowContext context, FormMessage errorMessage, boolean triggerBruteForce) {
     var email = MagicLink.getAttemptedUsername(context);
     sendOtp(context, email);
 
@@ -41,6 +44,17 @@ public class EmailOtpAuthenticator implements Authenticator {
     }
 
     Response response = form.createForm("otp-form.ftl");
+
+    if (triggerBruteForce) {
+      context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, response);
+      return;
+    }
+
+    if (errorMessage != null) {
+      context.forceChallenge(response);
+      return;
+    }
+
     context.challenge(response);
   }
 
@@ -51,15 +65,14 @@ public class EmailOtpAuthenticator implements Authenticator {
     }
     String code = String.format("%06d", ThreadLocalRandom.current().nextInt(999999));
     EventBuilder event = context.newEvent();
-    UserModel user =
-        MagicLink.getOrCreate(
-            context.getSession(),
-            context.getRealm(),
-            email,
-            isForceCreate(context, false),
-            false,
-            false,
-            MagicLink.registerEvent(event, EMAIL_OTP));
+    UserModel user = MagicLink.getOrCreate(
+        context.getSession(),
+        context.getRealm(),
+        email,
+        isForceCreate(context, false),
+        false,
+        false,
+        MagicLink.registerEvent(event, EMAIL_OTP));
 
     if (user == null) {
       log.debugf("User with email %s not found.", context.getUser().getEmail());
@@ -77,12 +90,22 @@ public class EmailOtpAuthenticator implements Authenticator {
   public void action(AuthenticationFlowContext context) {
     log.debug("EmailOtpAuthenticator.action");
 
+    UserModel user = context.getUser();
+    String bruteForceError = AuthenticatorUtils.getDisabledByBruteForceEventError(context, user);
+    if (bruteForceError != null) {
+      context.getEvent().user(user);
+      context.getEvent().error(bruteForceError);
+      challenge(context, new FormMessage(disabledByBruteForceError(bruteForceError)), false);
+      return;
+    }
+
     MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
     if (formData.containsKey("resend")) {
       context.getAuthenticationSession().removeAuthNote(USER_AUTH_NOTE_OTP_CODE);
-      challenge(context, null);
+      challenge(context, null, false);
       return;
     }
+
     String code = formData.getFirst(FORM_PARAM_OTP_CODE);
     log.debugf("Got %s for OTP code in form", code);
     try {
@@ -96,8 +119,9 @@ public class EmailOtpAuthenticator implements Authenticator {
     } catch (Exception e) {
       log.warn("Error comparing OTP code to form", e);
     }
-    context.getEvent().error(Errors.INVALID_USER_CREDENTIALS);
-    challenge(context, new FormMessage(Messages.INVALID_ACCESS_CODE));
+
+    context.getEvent().user(user).event(EventType.LOGIN_ERROR).error(Errors.INVALID_CODE);
+    challenge(context, new FormMessage(Messages.INVALID_ACCESS_CODE), true);
   }
 
   @Override
@@ -111,10 +135,19 @@ public class EmailOtpAuthenticator implements Authenticator {
   }
 
   @Override
-  public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {}
+  public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+  }
 
   @Override
-  public void close() {}
+  public void close() {
+  }
+
+  protected String disabledByBruteForceError(String error) {
+    if (Errors.USER_TEMPORARILY_DISABLED.equals(error)) {
+      return EmailOtpMessages.ACCOUNT_TEMPORARILY_DISABLED_EMAIL_OTP;
+    }
+    return EmailOtpMessages.ACCOUNT_PERMANENTLY_DISABLED_EMAIL_OTP;
+  }
 
   private boolean isForceCreate(AuthenticationFlowContext context, boolean defaultValue) {
     return is(context, CREATE_NONEXISTENT_USER_CONFIG_PROPERTY, defaultValue);
