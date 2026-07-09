9c7862619ef41fab8bbfd9a2e9d836315f387d72
nathanrockell5 <nathanrockell00@gmail.com>
2026-07-09 12:56:13 +0400

    test: add EmailOtpAuthenticator integration test (testcontainers + mailhog)

--- src/test/e2e/cypress/fixtures/uri.ts
+++ src/test/e2e/cypress/fixtures/uri.ts
@@ -7,10 +7,21 @@ const testRealmLoginUri = testRealmAuthUri.concat(
     '&redirect_uri=', testRealmUri.concat("/account")
 );
 
+const emailOtpRealmUri = Cypress.config('baseUrl') + "realms/email-otp-realm";
+const emailOtpRealmAuthUri = emailOtpRealmUri.concat("/protocol/openid-connect/auth");
+const emailOtpRealmLoginUri = emailOtpRealmAuthUri.concat(
+    '?response_type=code',
+    '&client_id=account',
+    '&scope=openid',
+    '&redirect_uri=', emailOtpRealmUri.concat("/account")
+);
+
 const mailhogBaseUrl = Cypress.env('mailhogUrl')
 
 export {
     testRealmLoginUri,
     testRealmUri,
+    emailOtpRealmLoginUri,
+    emailOtpRealmUri,
     mailhogBaseUrl,
 }
---
+++ src/test/java/io/phasetwo/keycloak/magic/web/EmailOtpSentInEmailTest.java
@@ -0,0 +1,42 @@
+package io.phasetwo.keycloak.magic.web;
+
+import lombok.extern.jbosslog.JBossLog;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.testcontainers.Testcontainers;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+@JBossLog
+@org.testcontainers.junit.jupiter.Testcontainers
+@EnabledIfSystemProperty(named = "include.cypress", matches = "true")
+public class EmailOtpSentInEmailTest extends AbstractMagicLinkWithMailhogTest {
+
+    @TestFactory
+    @DisplayName("Basic tests for the Email OTP authenticator")
+    public List<DynamicContainer> testEmailOtpAuthentication() throws IOException, InterruptedException, TimeoutException {
+        final var testRealm = setupTestKeycloakInstance();
+        assignEachUserAccountManagementRoles(testRealm);
+        final var client = keycloak
+                .realms()
+                .realm(testRealm.getRealm())
+                .clients()
+                .findByClientId("account")
+                .getFirst();
+        client.setName("Account Console");
+        keycloak.realms().realm(testRealm.getRealm()).clients().get(client.getId()).update(client);
+        return runCypressTests("cypress/e2e/email-otp.cy.ts", Map.of("MAILHOG_URL", "http://mailhog:8025"));
+    }
+
+    private RealmRepresentation setupTestKeycloakInstance() {
+        Testcontainers.exposeHostPorts(container.getHttpPort());
+        RealmRepresentation testRealm = importRealm("/realms/email-otp-basic-setup.json");
+        return testRealm;
+    }
+}
