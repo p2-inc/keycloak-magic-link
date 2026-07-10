e199d92378d3476c57fbfe88e829ebeb59c73e0d
nathanrockell5 <nathanrockell00@gmail.com>
2026-07-10 10:43:13 +0400

    Fixed indentation

--- src/test/java/io/phasetwo/keycloak/magic/web/EmailOtpSentInEmailTest.java
+++ src/test/java/io/phasetwo/keycloak/magic/web/EmailOtpSentInEmailTest.java
@@ -1,5 +1,9 @@
 package io.phasetwo.keycloak.magic.web;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
 import lombok.extern.jbosslog.JBossLog;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicContainer;
@@ -8,35 +12,33 @@ import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.testcontainers.Testcontainers;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeoutException;
-
 @JBossLog
 @org.testcontainers.junit.jupiter.Testcontainers
 @EnabledIfSystemProperty(named = "include.cypress", matches = "true")
 public class EmailOtpSentInEmailTest extends AbstractMagicLinkWithMailhogTest {
 
-    @TestFactory
-    @DisplayName("Basic tests for the Email OTP authenticator")
-    public List<DynamicContainer> testEmailOtpAuthentication() throws IOException, InterruptedException, TimeoutException {
-        final var testRealm = setupTestKeycloakInstance();
-        assignEachUserAccountManagementRoles(testRealm);
-        final var client = keycloak
-                .realms()
-                .realm(testRealm.getRealm())
-                .clients()
-                .findByClientId("account")
-                .getFirst();
-        client.setName("Account Console");
-        keycloak.realms().realm(testRealm.getRealm()).clients().get(client.getId()).update(client);
-        return runCypressTests("cypress/e2e/email-otp.cy.ts", Map.of("MAILHOG_URL", "http://mailhog:8025"));
-    }
+  @TestFactory
+  @DisplayName("Basic tests for the Email OTP authenticator")
+  public List<DynamicContainer> testEmailOtpAuthentication()
+      throws IOException, InterruptedException, TimeoutException {
+    final var testRealm = setupTestKeycloakInstance();
+    assignEachUserAccountManagementRoles(testRealm);
+    final var client =
+        keycloak
+            .realms()
+            .realm(testRealm.getRealm())
+            .clients()
+            .findByClientId("account")
+            .getFirst();
+    client.setName("Account Console");
+    keycloak.realms().realm(testRealm.getRealm()).clients().get(client.getId()).update(client);
+    return runCypressTests(
+        "cypress/e2e/email-otp.cy.ts", Map.of("MAILHOG_URL", "http://mailhog:8025"));
+  }
 
-    private RealmRepresentation setupTestKeycloakInstance() {
-        Testcontainers.exposeHostPorts(container.getHttpPort());
-        RealmRepresentation testRealm = importRealm("/realms/email-otp-basic-setup.json");
-        return testRealm;
-    }
+  private RealmRepresentation setupTestKeycloakInstance() {
+    Testcontainers.exposeHostPorts(container.getHttpPort());
+    RealmRepresentation testRealm = importRealm("/realms/email-otp-basic-setup.json");
+    return testRealm;
+  }
 }
